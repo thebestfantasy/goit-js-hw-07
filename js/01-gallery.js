dcbcb786ec5ef2a0bf5041e972f98ce42b39bd6a
thebestfantasy <sergej.fronch@gmail.com>
2023-04-10 23:04:49 +0300

    23

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,17 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const container = document.querySelector('.js-gallery');
+const markup = galleryItems.map(({ preview, original, description }) => `<li class="gallery__item"><a class="gallery__link" href="${original}"><img class="gallery__image" src="${preview}" data-source="${original}" alt="${description}"/></a></li>`);
+
+container.insertAdjacentHTML('beforeend', markup.join(''));
+container.addEventListener("click", onClick);
+
+console.log(container);
+
+function onClick(evt) {
+    evt.preventDefault();
+    console.log(evt.target);
+}
+
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,9 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const instance = basicLightbox.create(`
+    <h1>Dynamic Content</h1>
+    <p>You can set the content of the lightbox with JS.</p>
+`)
+instance.show();
